8909a25cc622b951544598adcd213128eb42625d
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-01-08 14:32:06 -0800

    Everything.

---
+++ main.js
@@ -0,0 +1,38 @@
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+// Submit the Form to Google Using "AJAX"
+<script data-cfasync="false" type="text/javascript" src="https://cdn.rawgit.com/dwyl/html-form-send-email-via-google-script-without-server/master/form-submission-handler.js"></script>
+// Add up and down arrows (spinner) to input fields
+// Add up and down arrows (spinner) to input fields
+<script>
+$( "#spinner" ).spinner();
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+<script>
+$('a[data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'bottom',
+    html: true
+});
+</script>
+
+<script>
+$( "#spinner" ).spinner();
+</script>
+<script>
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+<script>
+$('a[data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'bottom',
+    html: true
+});
+</script>
+</body>
